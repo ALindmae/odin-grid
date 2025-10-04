c5d8f0ecdab302cf42dde7dd217e779971424dfa
Amlindmae <113936984+ALindmae@users.noreply.github.com>
2025-10-04 12:40:21 -0300

    Refactor grid generation and support dynamic sizes
    - Turn grid generation logic into generateGrid(gridSize, squareSize) function
    - Add "Generate Grid" button that prompts user for grid size
    - Add getSquareSize() to calculate square size: max 50px, fit 960px
    - Change CSS container width to 960px; set square height to auto
    - Ensure squares are created dynamically

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let gridContainer = document.getElementById('grid-container');
+let gridSize;
 
-for (let i = 0; i <= 15;  i++) {
-    let gridRow = document.createElement('div');
-    gridRow.classList.add('grid-row');
-    gridRow.setAttribute('id', `row-${i}`);
+let genGridButton = document.querySelector('button');
+genGridButton.addEventListener('click', () => {
+    gridSize = Number(prompt("What size drawing board would you like? (max 100)"));
+    if (gridSize > 100) {
+        gridSize = Number(prompt("Too big. Define size smaller than 100"));
+    }
+    else if (gridSize < 1) {
+        gridSize = Number(prompt("Too small. Define size bigger than 1"));
+    }
     
-    for (let j = 0; j <= 15; j++) {
-        let gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridSquare.setAttribute('id', `square-${i}`);
-        gridRow.appendChild(gridSquare);
-    };
-    gridContainer.appendChild(gridRow);
-};
-
-
-gridContainer.addEventListener('mouseover', (e) =>  {
-    e.target.classList.add('active');
+    let squareSize = getSquareSize(gridSize);
+    generateGrid(gridSize, squareSize);
 });
+
+
+
+function generateGrid (gridSize, squareSize) {
+    let gridContainer = document.getElementById('grid-container');
+    gridContainer.replaceChildren();
+
+    for (let i = 0; i <= gridSize - 1;  i++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        gridRow.setAttribute('id', `row-${i}`);
+    
+        for (let j = 0; j <= gridSize - 1; j++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square');
+            gridSquare.setAttribute('id', `square-${i}`);
+            gridSquare.style.width = `${squareSize}px`;
+            gridRow.appendChild(gridSquare);
+        };
+
+    gridContainer.appendChild(gridRow);
+    };
+
+    gridContainer.addEventListener('mouseover', (e) =>  {
+    e.target.classList.add('active');
+});  
+}
+
+function getSquareSize (gridSize) {
+    return squareSize = (960 / gridSize) > 50 ? 50 : (960 / gridSize);
+}
