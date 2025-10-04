944cada3fc61e2f57ab17899d9de114edd971de1
Amlindmae <113936984+ALindmae@users.noreply.github.com>
2025-10-04 09:29:58 -0300

    Add JS grid of squares to HTML
    Fixed <script> bug in HTML: changed 'deref' to 'defer'

---
+++ script.js
@@ -0,0 +1,15 @@
+let gridContainer = document.getElementById('grid-container');
+
+for (let i = 0; i <= 15;  i++) {
+    let gridRow = document.createElement('div');
+    gridRow.classList.add('gridRow');
+    gridRow.setAttribute('id', `row-${i}`);
+    
+    for (let j = 0; j <= 15; j++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        gridSquare.setAttribute('id', `square-${i}`);
+        gridRow.appendChild(gridSquare);
+    };
+    gridContainer.appendChild(gridRow);
+};
