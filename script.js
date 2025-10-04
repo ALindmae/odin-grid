492b40af83ac023a22189fa74fdc5ca228853db5
Amlindmae <113936984+ALindmae@users.noreply.github.com>
2025-10-04 09:56:04 -0300

    Create css file to define grid styling

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let gridContainer = document.getElementById('grid-container');
 
 for (let i = 0; i <= 15;  i++) {
     let gridRow = document.createElement('div');
-    gridRow.classList.add('gridRow');
+    gridRow.classList.add('grid-row');
     gridRow.setAttribute('id', `row-${i}`);
     
     for (let j = 0; j <= 15; j++) {
         let gridSquare = document.createElement('div');
-        gridSquare.classList.add('gridSquare');
+        gridSquare.classList.add('grid-square');
         gridSquare.setAttribute('id', `square-${i}`);
         gridRow.appendChild(gridSquare);
     };
